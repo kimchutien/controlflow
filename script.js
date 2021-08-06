17f31918af443b3a21166853fc4aa7d29279f4cf
kimchutien <tienhm9083@hotmail.com>
2021-08-06 15:30:53 +0700

    loop update

---
+++ script.js
@@ -0,0 +1,12 @@
+/**For loop */
+// for(let i = 0; i < 5; i++){
+//     console.log('in loop: ', i);
+// }
+
+/**While loop */
+let i = 0;
+while(i < 5){
+    let html = `<div>${i}</div>`;
+    console.log(html);
+    i++;
+}
